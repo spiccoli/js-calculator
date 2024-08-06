140e6a1b471d6de33848ddad3e6683ad61e3e2f2
admin <piccolisantiagonicolas@gmail.com>
2024-08-06 20:40:37 -0300

    Calculator done, things to improve: 1-  Not using eval(), mainly because of safety reasons. 2- look for a  better way to handle errors

---
+++ index.js
@@ -0,0 +1,43 @@
+const display = document.getElementById("htmldisplay");
+
+setInterval(errorCheck, 700);
+
+function errorCheck() {
+    if (display.value === "Error" || display.value === undefined) {
+        clearDisplay();
+    }
+}
+
+function insertToDisplay(character) {
+  display.value += character;
+  display.scrollLeft = display.scrollWidth;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    const result = eval(display.value);
+    if (Math.abs(result) >= 1e7) {
+      // Convert to scientific notation with 2 decimal places
+      display.value = result.toExponential(2);
+    } else {
+      display.value = result;
+    }
+    // Scroll to the end of the input after calculation
+    display.scrollLeft = display.scrollWidth;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace" || event.key === "Delete") {
+    handleBackspace();
+  }
+});
+
+function handleBackspace() {
+  // Remove the last character from the display
+  display.value = display.value.slice(0, -1);
+}
